dbebd5873fe6084982eca0c2ed23c8724059136b
rajeshpola <raj2ps@gmail.com>
2025-03-31 23:29:33 +0530

    Add files via upload
    boiler plate code

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import db from "./utils/db.js";
+import cookieParser from "cookie-parser";
+//import all routes
+import userRoutes from "./routes/user.routes.js";
 
-const  sourcetextArea = document.getElementById("source");
-const  targettextArea = document.getElementById("target");
-const convertButton = document.getElementById("convert");
+dotenv.config();
 
-const convert = ()=>{
-   
-}
+const app = express();
 
-convertButton.addEventListener("click",convert)
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-console.log(convertButton)
+const port = process.env.PORT || 4000;
 
+app.get("/", (req, res) => {
+  res.send("Cohort!");
+});
+
+app.get("/hitesh", (req, res) => {
+  res.send("Hitesh");
+});
+
+app.get("/piyush", (req, res) => {
+  res.send("Piyush!");
+});
+
+//connect to db
+db();
+
+//user routes
+app.use("/api/v1/users", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
